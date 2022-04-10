47d776eb196effef49787f12a1aaa677b5244bd1
Murad Rahmanzada <97960285+MuradRahmanzada@users.noreply.github.com>
2022-04-10 19:52:29 +0400

    Update Firebase.js

--- src/component/Firebase/Firebase.js
+++ src/component/Firebase/Firebase.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
     measurementId: "G-0P12349WPQ"
 };
 
-console.log(process.env)
+
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
